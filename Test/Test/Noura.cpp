1a1cd9beefa26d493745fbb6f14142b63d8e774c
NAJ3 <nourajodeyeh@gmail.com>
2018-12-20 09:30:50 +0200

    after new branch

--- Test/Test/Noura.cpp
+++ Test/Test/Noura.cpp
@@ -7,5 +7,6 @@ void main() {
 	printf("The third time");
 	printf("The fourth time");
 	printf("The fifth time after remote connection");
+	printf("after creating branchTest this commit will be added to it");
 
 }
